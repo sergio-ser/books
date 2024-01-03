bd66a0ba64b669354d2f71b9aed5578f2a1f17b1
Sergiu Chetrar <120366864+SergiuChetrar@users.noreply.github.com>
2024-01-03 15:55:00 +0200

    updates

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use Faker\Factory;
+use App\Entity\Books;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+        $batchSize = 1000;
+
+        for ($i = 0; $i < 1000000; $i++) {
+            $entity = new Books;
+            $entity->setTitle($faker->sentence(4));
+            $entity->setAuthor($faker->name);
+            $entity->setDescription($faker->paragraph);
+            $entity->setPrice($faker->randomFloat(2, 1, 100));
+
+            $manager->persist($entity);
+
+            if (($i % $batchSize) === 0) {
+                $manager->flush();
+                $manager->clear();
+                $manager->detach($entity);
+                gc_collect_cycles();
+            }
+        }
+
+        $manager->flush();
+        gc_collect_cycles();
+    }
+}
